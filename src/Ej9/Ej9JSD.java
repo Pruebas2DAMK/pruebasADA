3e5d7435f71145f3e4a0300245626f0c034efeb1
JoelS <joelsempere96@gmail.com>
2021-10-01 20:34:18 +0200

    Ejercicio 9 hecho, añadir comentarios si acaso. Crear metodos para cada funcionalidad e implementar en el main

--- src/Ej9/Ej9JSD.java
+++ src/Ej9/Ej9JSD.java
@@ -1,14 +1,40 @@
 package Ej9;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 
 public class Ej9JSD {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] titols = {
+                "Les tenebres i l'alba",
+                "El Quixot de la Mancha",
+                "El Senyor del Anells",
+                "El petit princep"
+        };
+        int[] anyoPublicacion = {
+                2020,
+                1605,
+                1954,
+                1943
+        };
+        String ruta = "./BinaryFile.dat";
         try {
-            DataOutputStream dos = new DataOutputStream(new FileOutputStream(new File("./BinaryFile.dat")));
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(ruta));
+            for (int i = 0; i <titols.length ; i++) {
+                dos.writeUTF(titols[i]);
+                dos.writeInt(anyoPublicacion[i]);
+            }
+            dos.close();
+        }catch (Exception e){
+
+        }
+        try {
+            DataInputStream dis = new DataInputStream(new FileInputStream(ruta));
+            for (int i = 0; i < titols.length ; i++) {
+                System.out.println(dis.readUTF());
+                System.out.println(dis.readInt());
+            }
+            dis.close();
         }catch (Exception e){
 
         }
