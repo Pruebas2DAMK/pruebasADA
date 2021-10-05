1b690da8404d17c71bad74f088153ea408e2df93
JoelS <joelsempere96@gmail.com>
2021-10-05 19:57:18 +0200

    Ejercicio 10 hecho.

--- src/Ej10/Book.java
+++ src/Ej10/Book.java
@@ -8,13 +8,37 @@ Joel Sempere Durá
 
  */
 public class Book implements Serializable {
+
 String author;
 String name;
 String editorial;
+Date date;
+
+    Book(String author,String name,String editorial,Date date){
+        this.author=author;
+        this.name=name;
+        this.editorial=editorial;
+        this.date=date;
+    }
+
+    public Book(String author, String name, String editorial) {
+        this.author = author;
+        this.name = name;
+        this.editorial = editorial;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro {" +"\n"+
+                "Autor = " + author + "\n" +
+                ", Nombre = " + name + "\n" +
+                ", Editorial = " + editorial + "\n" +
+                ", Date = " + date +
+                "\n}";
+    }
 /*
 (año,mes,dia)
 Año a partir de 1900, 2021 seria 121
 El mes empieza a partir de 0
  */
-Date date;
 }
--- src/Ej10/Ej10JSD.java
+++ src/Ej10/Ej10JSD.java
@@ -1,4 +1,31 @@
 package Ej10;
 
+import java.io.*;
+import java.util.Date;
+
 public class Ej10JSD {
+
+    static void escribeObjeto(Object b,File f) throws IOException {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f))){
+            oos.writeObject(b);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    static Object leeObjecto(File f){
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f))){
+            return ois.readObject();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        File f = new File ("./BookFile.dat");
+        Book nuevo = new Book("Joel Sempere","Mi libro de prueba","Editorial mi prueba",new Date(121,7,28));
+        escribeObjeto(nuevo,f);
+        System.out.println(leeObjecto(f).toString());
+    }
+
 }
